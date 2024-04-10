6cc89147955de7b59a22e0e5c5bc48b3a32fef27
mirolim725 <mirolimsherboboyev@gmail.com>
2024-04-10 11:00:23 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,164 @@
+
+// FOR-1
+
+// let k=5,
+// n = 3;
+
+
+
+// for (let i = 0; i < n ; i++) {
+//     console.log(k);
+// }
+
+
+
+// FOR-2
+
+// let a = 12 , b = 5 , count=0 ;
+
+// if (a < b) {
+//     for (let i = a ; i <= b; i++) {
+//         console.log(i);
+//         count++
+//     }
+// } else{
+//     for (let i = b; i <= a; i++) {
+//         console.log(i);
+//         count++
+//     }
+// }
+// console.log(count);
+
+
+
+
+// FOR-3
+
+
+// let a = 1 , b = 5 , count=0 ;
+
+// if (a < b) {
+//     for (let i = b - 1  ; i > a; i--) {
+//         console.log(i);
+//         count++
+//     }
+// } else{
+//     for (let i = a - 1 ; i > b; i--) {
+//         console.log(i);
+//         count++
+//     }
+// }
+// console.log(count);
+
+
+
+// FOR-4
+
+
+
+// let a = 1 , b = 10 , c = 100 ;
+
+// for (let i = a ; i <= b ; i++) {
+//     let narx = i * c
+//     console.log(`${i} kilogramm konfetning narxi ${narx} so'm`);
+// }
+
+
+// FOR-5
+
+
+// let a = 1 , b = 10, c = 100 ;
+
+// for (let i = a ; i <= b ; i++ ) {
+//     let narx = (i / 10) * c
+//     console.log(`${i / 10} kilogramm konfetning narxi ${narx} so'm`);
+// }
+
+
+
+// FOR-6
+
+
+// let a = 10 , b = 20, c = 100 ;
+
+// for (let i = a ; i <= b ; i+=2 ) {
+//     let narx = (i / 10) * c
+//     console.log(`${i / 10} kilogramm konfetning narxi ${narx} so'm`);
+// }
+
+
+// FOR-7
+
+
+
+
+// let a = 1 , b = 100 , count=0 ;
+
+// if (a < b) {
+//     for (let i = a ; i <= b; i++) {  
+//         count+=i
+//     }
+// } else{
+//     for (let i = b; i <= a; i++) {
+//         count+=i  
+//     }
+// }
+// console.log(count);
+
+
+
+
+// FOR-8
+
+
+
+
+// let a = 1 , b = 100 , count=1 ;
+
+// if (a < b) {
+//     for (let i = a ; i <= b; i++) {  
+//         count*=i
+//     }
+// } else{
+//     for (let i = b; i <= a; i++) {
+//         count*=i  
+//     }
+// }
+// console.log(count);
+
+
+
+
+// FOR-9
+
+
+
+
+// let a = 1 , b = 4 , count=0 ;
+
+// if (a < b) {
+//     for (let i = a ; i <= b; i++) {  
+//         let result= i*i
+//         count+=result
+//     }
+// } else{
+//     for (let i = b; i <= a; i++) {
+//         let result= i*i
+//         count+=result
+//     }
+// }
+// console.log(count);
+
+
+
+// FOR-10
+
+
+
+
+// let n = 2 , count = 0;
+
+// for (let i = 1; i <= n; i++) {
+//     count+=1/i
+// }
+// console.log(count);
